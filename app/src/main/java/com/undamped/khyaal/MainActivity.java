aec3f0831cb7b1d1faaf38b73b9477eab890936d
Sai Sukesh <sukesh.sai04@gmail.com>
2021-04-03 13:00:23 +0530

    Added Image Selection

--- app/src/main/java/com/undamped/khyaal/MainActivity.java
+++ app/src/main/java/com/undamped/khyaal/MainActivity.java
@@ -1,17 +1,58 @@
 package com.undamped.khyaal;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+import static java.security.AccessController.getContext;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.add_event_photo) CardView add_event_photo;
+    @BindView(R.id.prescriptionImageView) ImageView prescriptionImageView;
+
+    final public static int IMAGE_CODE = 1;
+    private Uri imageUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ButterKnife.bind(this);
 
+        add_event_photo.setOnClickListener(view -> {
+            selectImage();
+        });
+    }
+
+    private void selectImage() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, IMAGE_CODE);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == IMAGE_CODE && resultCode == RESULT_OK && data != null) {
+            imageUri = data.getData();
+            Glide.with(getApplicationContext()).load(imageUri).into(prescriptionImageView);
+        } else {
+            Toast.makeText(getApplicationContext(), "Please select a file", Toast.LENGTH_SHORT).show();
+        }
     }
 }
