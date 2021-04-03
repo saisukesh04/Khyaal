a8ab9b9765314496b12bb61e31c2df078728653f
Prasanna Venkatesh <prasannavenkatesh.ramkumar@iiitb.org>
2021-04-03 18:22:00 +0530

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,37 @@
+import discord
+from discord.ext import commands 
+import random 
+import requests
+from datetime import date 
+
+
+import keys
+
+
+client = commands.Bot(command_prefix= '.')
+
+
+
+@client.event
+async def on_ready():
+    print("Khyaal Bot is ready")
+
+
+@client.command()
+async def ping(ctx):
+    await ctx.send(f'Pong! Latency = {client.latency * 1000} ms')
+
+@client.command()
+async def hi(ctx):
+    await ctx.send(f'Hi!')
+
+@client.command()
+async def test(ctx, arg1, arg2):
+    url1 = 'https://api.edamam.com/api/food-database/v2/parser?ingr={}&app_id={}&app_key={}'.format(arg1,keys.ID,keys.APP_KEY)
+    url2 = 'https://api.edamam.com/api/food-database/v2/parser?ingr={}&app_id={}&app_key={}'.format(arg2,keys.ID,keys.APP_KEY)
+    print(requests.get(url1).status_code)
+    print(requests.get(url2).text)
+    await ctx.send('You passed {} and {}'.format(arg1, arg2))
+
+
+client.run(keys.botToken)
---
+++ website/script.js
@@ -0,0 +1,6 @@
+"use strict";
+
+console.log("Script is working properly");
+
+const message = "Check our Repo";
+document.querySelector(".repo").textContent = message;
