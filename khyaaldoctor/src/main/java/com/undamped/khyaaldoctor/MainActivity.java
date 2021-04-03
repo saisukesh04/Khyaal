f69b57d7407cb69b27fd0535687ae6efa876016c
Sai Sukesh <sukesh.sai04@gmail.com>
2021-04-03 16:58:03 +0530

    Completed Doctor Login & Register

--- khyaaldoctor/src/main/java/com/undamped/khyaaldoctor/MainActivity.java
+++ khyaaldoctor/src/main/java/com/undamped/khyaaldoctor/MainActivity.java
@@ -5,13 +5,31 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        startActivity(new Intent(MainActivity.this, RegisterActivity.class));
+        mAuth = FirebaseAuth.getInstance();
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        if (mAuth.getCurrentUser() == null) {
+            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
+            finish();
+        }
+
     }
 }
--- khyaaldoctor/src/main/java/com/undamped/khyaaldoctor/RegisterActivity.java
+++ khyaaldoctor/src/main/java/com/undamped/khyaaldoctor/RegisterActivity.java
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
@@ -13,6 +15,11 @@ import android.widget.Toast;
 
 import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -42,9 +49,17 @@ public class RegisterActivity extends AppCompatActivity {
         });
 
         registerBtn.setOnClickListener(v -> {
-            String email = emailREditText.getText().toString();
+            InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+            View focusedView = RegisterActivity.this.getCurrentFocus();
+            if (focusedView != null) {
+                imm.hideSoftInputFromWindow(focusedView.getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+            }
+
+            String email = emailREditText.getText().toString().trim();
             String password = passwordREditText.getText().toString();
             String confPassword = confirmPassEditText.getText().toString();
+            String name = userName.getText().toString().trim();
+            String regNo = registrationID.getText().toString().trim();
 
             if (email.isEmpty() || password.isEmpty() || confPassword.isEmpty())
                 Snackbar.make(v, "Please fill all the fields", Snackbar.LENGTH_LONG).show();
@@ -59,13 +74,29 @@ public class RegisterActivity extends AppCompatActivity {
                         .addOnCompleteListener(RegisterActivity.this, task -> {
                             if (task.isSuccessful()) {
                                 Toast.makeText(RegisterActivity.this, "Registration successful. Logging in", Toast.LENGTH_LONG).show();
-                                startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
-                                finish();
+                                storeUserDataToDatabase(name, regNo);
                             } else {
                                 regProgressBar.setVisibility(View.INVISIBLE);
                                 Toast.makeText(RegisterActivity.this, "Registration failed. Try again", Toast.LENGTH_LONG).show();
+                                Log.e("Error: Register", "Error while adding data to database\n" + task.getException());
                             }
                         });
+            }
+        });
+    }
+
+    private void storeUserDataToDatabase(String name, String regNo) {
+        DatabaseReference mRef = FirebaseDatabase.getInstance().getReference("Doctors");
+        Map<String, String> userMap = new HashMap<>();
+        userMap.put("Name", name);
+        mRef.child(regNo).setValue(userMap).addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                startActivity(new Intent(RegisterActivity.this, MainActivity.class));
+                finish();
+            }else{
+                regProgressBar.setVisibility(View.INVISIBLE);
+                Toast.makeText(RegisterActivity.this, "Registration failed. Try again", Toast.LENGTH_LONG).show();
+                Log.e("Error: Register", "Error while adding data to database\n" + task.getException());
             }
         });
     }
