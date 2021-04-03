5d055180fbfc849ddcaeb37010b43ec0ab157410
Sai Sukesh <sukesh.sai04@gmail.com>
2021-04-03 14:42:04 +0530

    Success in Processing image

--- app/src/main/java/com/undamped/khyaal/MainActivity.java
+++ app/src/main/java/com/undamped/khyaal/MainActivity.java
@@ -3,30 +3,39 @@ package com.undamped.khyaal;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
+import androidx.core.app.NotificationCompat;
 
 import android.content.Intent;
+import android.graphics.Point;
+import android.graphics.Rect;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.Task;
+import com.google.mlkit.vision.common.InputImage;
+import com.google.mlkit.vision.text.Text;
+import com.google.mlkit.vision.text.TextRecognition;
+import com.google.mlkit.vision.text.TextRecognizer;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-import static java.security.AccessController.getContext;
-
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     @BindView(R.id.add_event_photo) CardView add_event_photo;
-    @BindView(R.id.prescriptionImageView) ImageView prescriptionImageView;
+    @BindView(R.id.prescriptionImageView)
+    ImageView prescriptionImageView;
 
     final public static int IMAGE_CODE = 1;
     private Uri imageUri; // URI of the image to be processed
-    private ArrayList<String>prescription;
+    private ArrayList<String> prescription;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,11 +63,12 @@ public class MainActivity extends AppCompatActivity {
         if (requestCode == IMAGE_CODE && resultCode == RESULT_OK && data != null) {
             imageUri = data.getData();
             Glide.with(getApplicationContext()).load(imageUri).into(prescriptionImageView);
-           
-}
-
-
-         else {
+            try {
+                processImage();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
             Toast.makeText(getApplicationContext(), "Please select a file", Toast.LENGTH_SHORT).show();
         }
     }
@@ -66,80 +76,73 @@ public class MainActivity extends AppCompatActivity {
     private void createNotificationChannel() {
         // Create the NotificationChannel, but only on API 26+ because
         // the NotificationChannel class is new and not in the support library
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            CharSequence name = getString(R.string.channel_name);
-            String description = getString(R.string.channel_description);
-            int importance = NotificationManager.IMPORTANCE_DEFAULT;
-            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
-            channel.setDescription(description);
-            // Register the channel with the system; you can't change the importance
-            // or other notification behaviors after this
-            NotificationManager notificationManager = getSystemService(NotificationManager.class);
-            notificationManager.createNotificationChannel(channel);
-        }
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+//            CharSequence name = getString(R.string.channel_name);
+//            String description = getString(R.string.channel_description);
+//            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+//            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+//            channel.setDescription(description);
+//            // Register the channel with the system; you can't change the importance
+//            // or other notification behaviors after this
+//            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+//            notificationManager.createNotificationChannel(channel);
+//        }
     }
 
-    private void setTapAction(){
-          // Create an explicit intent for an Activity in your app
-        Intent intent = new Intent(this, AlertDetails.class);
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
-
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
-                .setSmallIcon(R.drawable.notification_icon)
-                .setContentTitle("My notification")
-                .setContentText("Hello World!")
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-                // Set the intent that will fire when the user taps the notification
-                .setContentIntent(pendingIntent)
-                .setAutoCancel(false);
-
+    private void setTapAction() {
+        // Create an explicit intent for an Activity in your app
+//        Intent intent = new Intent(this, AlertDetails.class);
+//        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+//        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
+//
+//        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+//                .setSmallIcon(R.drawable.notification_icon)
+//                .setContentTitle("My notification")
+//                .setContentText("Hello World!")
+//                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+//                // Set the intent that will fire when the user taps the notification
+//                .setContentIntent(pendingIntent)
+//                .setAutoCancel(false);
     }
 
-  
-    
 
-    private void processImage(){
+    private void processImage() throws IOException {
+        prescription = new ArrayList<>();
         TextRecognizer recognizer = TextRecognition.getClient();
-        Task<Text> result =
-        recognizer.process(this.imageUri) // To be replaced with data?
-                .addOnSuccessListener(new OnSuccessListener<Text>() {
-                    @Override
-                    public void onSuccess(Text visionText) {
-                        // Task completed successfully
-                        for (Text.TextBlock block : visionText.getTextBlocks()) {
-                            Rect boundingBox = block.getBoundingBox();
-                            Point[] cornerPoints = block.getCornerPoints();
-                            String text = block.getText();
-                            prescription.add(text);
-                            //for (Text.Line line: block.getLines()) {
-                                // ...
-                              //  for (Text.Element element: line.getElements()) {
-                                    // ...
-                                //}
-                            //}
-                        }
+        InputImage inputImage = InputImage.fromFilePath(getApplicationContext(), imageUri);
+        Task<Text> result = recognizer.process(inputImage) // To be replaced with data?
+                .addOnSuccessListener(visionText -> {
+                    // Task completed successfully
+                    Log.e("Info", "Scanned");
+                    for (Text.TextBlock block : visionText.getTextBlocks()) {
+                        Rect boundingBox = block.getBoundingBox();
+                        Point[] cornerPoints = block.getCornerPoints();
+                        String text = block.getText();
+                        Log.e("Info", text);
+                        prescription.add(text);
+                        //for (Text.Line line: block.getLines()) {
+                        // ...
+                        //  for (Text.Element element: line.getElements()) {
+                        // ...
+                        //}
+                        //}
                     }
                 })
-                .addOnFailureListener(
-                        new OnFailureListener() {
-                            @Override
-                            public void onFailure(@NonNull Exception e) {
-                                // Task failed with an exception
-                                // ...
-                            }
-                        });
+                .addOnFailureListener(e -> {
+                    // Task failed with an exception
+                    Log.e("Error", e.getMessage());
+                });
 // [END run_detector]
     }
 
-    private void processTextBlock(){
+    private void processTextBlock() {
         ArrayList<String> toBeNotified = this.prescription;
-        for(String s: toBeNotified){
-            NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
-            .setSmallIcon(R.drawable.notification_icon)
-            .setContentTitle("Notification boss")
-            .setContentText(s)
-            .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+        for (String s : toBeNotified) {
+            NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "0")
+                    .setSmallIcon(R.drawable.taking_care)
+                    .setContentTitle("Notification boss")
+                    .setContentText(s)
+                    .setPriority(NotificationCompat.PRIORITY_MAX);
             /*
             NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
 
@@ -147,8 +150,8 @@ public class MainActivity extends AppCompatActivity {
             notificationManager.notify(notificationId, builder.build());
 
             */
+
+
         }
-
     }
-
 }
--- app/src/main/java/com/undamped/khyaal/RegisterActivity.java
+++ app/src/main/java/com/undamped/khyaal/RegisterActivity.java
@@ -2,13 +2,67 @@ package com.undamped.khyaal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.FirebaseAuth;
+
+import butterknife.BindView;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    @BindView(R.id.userName) EditText userName;
+    @BindView(R.id.emailREditText) EditText emailREditText;
+    @BindView(R.id.passwordREditText) EditText passwordREditText;
+    @BindView(R.id.confirmPassEditText) EditText confirmPassEditText;
+
+    @BindView(R.id.registerBtn) Button registerBtn;
+    @BindView(R.id.clickLogin) TextView clickLogin;
+    @BindView(R.id.regProgressBar) ProgressBar regProgressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        clickLogin.setOnClickListener(view -> {
+            startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+            finish();
+        });
+
+        registerBtn.setOnClickListener(v -> {
+            String email = emailREditText.getText().toString();
+            String password = passwordREditText.getText().toString();
+            String confPassword = confirmPassEditText.getText().toString();
+
+            if (email.isEmpty() || password.isEmpty() || confPassword.isEmpty())
+                Snackbar.make(v, "Please fill all the fields", Snackbar.LENGTH_LONG).show();
+            else if (password.length() < 8)
+                Snackbar.make(v, "Password should contain at least 8 characters", Snackbar.LENGTH_LONG).show();
+            else if (!confPassword.equals(password))
+                Snackbar.make(v, "Passwords don't match", Snackbar.LENGTH_LONG).show();
+            else {
+                FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+                regProgressBar.setVisibility(View.VISIBLE);
+                firebaseAuth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(RegisterActivity.this, task -> {
+                            if (task.isSuccessful()) {
+                                Toast.makeText(RegisterActivity.this, "Registration successful. Logging in", Toast.LENGTH_LONG).show();
+                                startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+                                finish();
+                            } else {
+                                regProgressBar.setVisibility(View.INVISIBLE);
+                                Toast.makeText(RegisterActivity.this, "Registration failed. Try again", Toast.LENGTH_LONG).show();
+                            }
+                        });
+            }
+        });
     }
 }
