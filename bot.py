b3687b6c5b97ecc017706b310c5ec9f9fc956868
Prasanna Venkatesh <prasannavenkatesh.ramkumar@iiitb.org>
2021-04-03 23:07:55 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -31,7 +31,11 @@ async def test(ctx, arg1, arg2):
     url2 = 'https://api.edamam.com/api/food-database/v2/parser?ingr={}&app_id={}&app_key={}'.format(arg2,keys.ID,keys.APP_KEY)
     print(requests.get(url1).status_code)
     print(requests.get(url2).text)
-    await ctx.send('You passed {} and {}'.format(arg1, arg2))
+    x = requests.get(url2).json()
+    z = requests.get(url1).json()
+    y = x['parsed'][0]['food']['nutrients']
+    k = z['parsed'][0]['food']['nutrients']
+    await ctx.send(f'{y},\n {k}')
 
 
 client.run(keys.botToken)
